43aec265eb0b5a58bfb57e086126c298bd12fcf8
Petr Hasoň <petrhason@gmail.com>
2022-08-05 16:44:32 +0200

    cetnost_graf

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ email: petrhason@gmail.com
 discord: Petr H. #4342
 """
 from task_template import TEXTS
-
-users = {'bob': '123',
-         'ann': 'pass123',
-         'mike': 'password123',
-         'liz': 'pass123',
-         'renata': 'heslo'
-         }
-
-username = input('Zadej přihlašovací jméno: ')
-password = input('Zadej heslo: ')
-
-if users.get(username) == password:
-    print(f'Ahoj {username.upper()} pojďme analyzovat text...')
-
-else:
-    print(f'Neplatné uživatelské jméno nebo heslo.')
-    quit()
+oddelovac = ('----------------------------------------')
+# users = {'bob': '123',
+#          'ann': 'pass123',
+#          'mike': 'password123',
+#          'liz': 'pass123',
+#          'renata': 'heslo'
+#          }
+#
+# username = input('Zadej přihlašovací jméno: ')
+# password = input('Zadej heslo: ')
+#
+# if users.get(username) == password:
+#     print(f'Ahoj {username.upper()} pojďme analyzovat text...')
+#
+# else:
+#     print(f'Neplatné uživatelské jméno nebo heslo.')
+#     quit()
 
 
 cislo_textu = input(f'Zvol od 1 do 3 číslo textu pro analýzu: ')
@@ -33,7 +33,7 @@ if cislo_textu.isnumeric() and 1 <= int(cislo_textu) <= 3:
 else:
     print(f'Chybné zadání. Ukončuji program.')
 
-print(f'Vybraný text: \n{vybrany_text} \n')
+# print(f'Vybraný text: \n{vybrany_text} \n')
 
 # počet slov
 pocet_slov = len(vybrany_text.split())
@@ -71,3 +71,39 @@ print(f'Slova psané VELKÝMI písmeny: {velka_pismena}')
 print(f'Slova psané malými písmeny: {mala_pismena}')
 print(f'Počet číslic: {pocet_cisel}')
 print(f'Suma čísel: {sum(list_cisel)}')
+
+#  četnost různých délek slov v textu
+
+clear_slova = []
+for word in vybrany_text.split():
+    clear_slova.append(word.strip(',.:;').lower())
+
+pocet_znaku = []
+for znaky in clear_slova:
+    pocet_znaku.append(len(znaky))
+
+cetnost = {}
+for index in pocet_znaku:
+    if index not in cetnost:
+        cetnost[index] = 1
+    else:
+        cetnost[index] += 1
+# print(cetnost)
+
+print(f'{oddelovac}\n'
+      f'LEN|  OCCURENCES |NR. \n'
+      f'{oddelovac}')
+
+for key, value in sorted(cetnost.items()):
+    print(f'{key:3}|{"*" * value:12} |{value}')
+
+
+
+# for key, value in cetnost:
+# print(key, value)
+
+
+
+
+
+
